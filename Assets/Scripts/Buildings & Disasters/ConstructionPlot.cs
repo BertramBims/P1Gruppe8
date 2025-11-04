90b28bdfc652f31eeddb6e440c23943f23b482e9
Bertram Winter <bertram.winter004@gmail.com>
2025-11-04 12:27:49 +0100

    stuff + tmp essentials

--- Assets/Scripts/Buildings & Disasters/ConstructionPlot.cs
+++ Assets/Scripts/Buildings & Disasters/ConstructionPlot.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public class ConstructionPlot : MonoBehaviour
 {
-    public BuildingInstance currentBuilding;
+    public GameObject buildUIPrefab;
 
     public void OnMouseDown()
     {
-        if (currentBuilding == null)
-        {
-            BuildManager.Instance.TryBuildAt(this);
-        } else
-        {
-            // later open up UI
-        }
+        //open ui here
+        if (buildUIPrefab.activeInHierarchy == true)
+            buildUIPrefab.SetActive(false);
+        if (buildUIPrefab.activeInHierarchy == false)
+            buildUIPrefab.SetActive(true);
+    }
+
+    public void SelectBuilding(BuildingType type)
+    {
+        BuildManager.Instance.SelectBuildingType(type);
+    }
+
+    public void TryBuild()
+    {
+        BuildManager.Instance.TryBuildAt(this);
     }
 }
