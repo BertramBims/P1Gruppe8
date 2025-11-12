50d645dd411d99aab76c3620f4093edadb2c4cc4
sebEFN <sebastian.efnielsen@gmail.com>
2025-11-12 11:47:16 +0100

    fixed mouseintereact

--- Assets/Scripts/mouseInteract.cs
+++ Assets/Scripts/mouseInteract.cs
@@ -9,7 +9,6 @@ using UnityEngine.UIElements;
 
 public class mouseInteract : MonoBehaviour
 {
-
     void ApplyRedToChildren(){
         SpriteRenderer[] childRenderers = GetComponentsInChildren<SpriteRenderer>();
         for (int i = 0; i < childRenderers.Length; i++)
@@ -31,8 +30,16 @@ public class mouseInteract : MonoBehaviour
                  childRenderers[i].color = new Color (0, 0, 0, 255);
         }
     }
+
+    void ApplyredOnTag()
+    {
+        GameObject tagRenderers = GameObject.FindWithTag("OutlineBlack");
+        SpriteRenderer spriteRenderer = tagRenderers.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.red;
+    }
     private void OnMouseEnter(){
-        ApplyRedToChildren();
+        //ApplyRedToChildren();
+        ApplyredOnTag();
     }
 
     private void OnMouseExit(){
