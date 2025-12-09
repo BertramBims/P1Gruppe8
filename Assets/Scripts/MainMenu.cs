8b816cb1fa94a4063db4d34077bb3cb7e08c6d79
sebEFN <94054113+sebEFN@users.noreply.github.com>
2025-12-09 14:47:00 +0100

    fixed main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3); 
     }
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void PlayTutorial()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4); 
